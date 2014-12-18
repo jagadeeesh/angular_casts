b903db73ad3f6b465687e104717355cfd8fa827a
jagadeeesh <k.jagadeesh123@gmail.com>
2014-12-18 23:14:19 +0530

    Added angular CDN

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :screencasts
+
+  scope :api do
+    get "/screencasts(.:format)" => "screencasts#index"
+    get "/screencasts/:id(.:format)" => "screencasts#show"
+  end
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
